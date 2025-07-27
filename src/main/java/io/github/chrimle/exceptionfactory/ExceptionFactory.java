06fc3dd3d8323f629c8cafc7f14f793706cb3cc1
Chrimle <28791817+Chrimle@users.noreply.github.com>
2025-07-27 23:16:30 +0200

    Add `ExceptionFactory`-class

---
+++ src/main/java/io/github/chrimle/exceptionfactory/ExceptionFactory.java
@@ -0,0 +1,12 @@
+package io.github.chrimle.exceptionfactory;
+
+/**
+ * <em>Factory</em> of {@link RuntimeException}s and <em>sub-classes</em>.
+ *
+ * @author Chrimle
+ * @since 0.1.0
+ */
+public final class ExceptionFactory {
+
+  private ExceptionFactory() {}
+}
