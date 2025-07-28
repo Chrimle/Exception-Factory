739f4324d2c8b7b05251c673ae2ea3181c4cf38d
Chrimle <28791817+Chrimle@users.noreply.github.com>
2025-07-28 13:34:59 +0200

    Add `ExceptionFactory`-methods

--- src/main/java/io/github/chrimle/exceptionfactory/ExceptionFactory.java
+++ src/main/java/io/github/chrimle/exceptionfactory/ExceptionFactory.java
@@ -1,12 +1,104 @@
 package io.github.chrimle.exceptionfactory;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 /**
- * <em>Factory</em> of {@link RuntimeException}s and <em>sub-classes</em>.
+ * <em>Factory</em> of {@link Exception}s and <em>sub-classes</em>.
  *
+ * @see ExceptionBuilder
  * @author Chrimle
  * @since 0.1.0
  */
 public final class ExceptionFactory {
 
   private ExceptionFactory() {}
+
+  private static <C extends Exception> C exceptionOf(
+      final Class<C> exceptionClass,
+      @Nullable final String key,
+      final MessageTemplates.OneArgTemplate messageTemplate) {
+    if (messageTemplate == null) {
+      throw new IllegalArgumentException("`messageTemplate` MUST NOT be `null`");
+    }
+    return ExceptionBuilder.of(exceptionClass).setMessage(messageTemplate, key).build();
+  }
+
+  private static <C extends Exception> C exceptionOf(
+      final Class<C> exceptionClass,
+      @Nullable final String key,
+      final MessageTemplates.TwoArgTemplate messageTemplate,
+      final String value) {
+    if (messageTemplate == null) {
+      throw new IllegalArgumentException("`messageTemplate` MUST NOT be `null`");
+    }
+    return ExceptionBuilder.of(exceptionClass).setMessage(messageTemplate, key, value).build();
+  }
+
+  /**
+   * Creates an {@link IllegalArgumentException} instance with the {@code message} derived from the
+   * {@code messageTemplate} and {@code key}.
+   *
+   * @param key to include in the {@code messageTemplate}.
+   * @param messageTemplate to create the {@code message}.
+   * @return a new {@link IllegalArgumentException} instance.
+   * @throws IllegalArgumentException if {@code messageTemplate} is {@code null}.
+   */
+  @Contract("_, null -> fail; _, _ -> new")
+  public static @NotNull IllegalArgumentException illegalArgumentOf(
+      @Nullable final String key, final MessageTemplates.OneArgTemplate messageTemplate) {
+    return exceptionOf(IllegalArgumentException.class, key, messageTemplate);
+  }
+
+  /**
+   * Creates an {@link IllegalArgumentException} instance with the {@code message} derived from the
+   * {@code messageTemplate} and {@code key}.
+   *
+   * @param key to include in the {@code messageTemplate}.
+   * @param messageTemplate to create the {@code message}.
+   * @param value to include in the {@code messageTemplate}.
+   * @return a new {@link IllegalArgumentException} instance.
+   * @throws IllegalArgumentException if {@code messageTemplate} is {@code null}.
+   */
+  @Contract("_, null, _ -> fail; _, _, _ -> new")
+  public static @NotNull IllegalArgumentException illegalArgumentOf(
+      @Nullable final String key,
+      final MessageTemplates.TwoArgTemplate messageTemplate,
+      @Nullable final String value) {
+    return exceptionOf(IllegalArgumentException.class, key, messageTemplate, value);
+  }
+
+  /**
+   * Creates an {@link IllegalStateException} instance with the {@code message} derived from the
+   * {@code messageTemplate} and {@code key}.
+   *
+   * @param key to include in the {@code messageTemplate}.
+   * @param messageTemplate to create the {@code message}.
+   * @return a new {@link IllegalStateException} instance.
+   * @throws IllegalArgumentException if {@code messageTemplate} is {@code null}.
+   */
+  @Contract("_, null -> fail; _, _ -> new")
+  public static @NotNull IllegalStateException illegalStateOf(
+      @Nullable final String key, final MessageTemplates.OneArgTemplate messageTemplate) {
+    return exceptionOf(IllegalStateException.class, key, messageTemplate);
+  }
+
+  /**
+   * Creates an {@link IllegalStateException} instance with the {@code message} derived from the
+   * {@code messageTemplate} and {@code key}.
+   *
+   * @param key to include in the {@code messageTemplate}.
+   * @param messageTemplate to create the {@code message}.
+   * @param value to include in the {@code messageTemplate}.
+   * @return a new {@link IllegalStateException} instance.
+   * @throws IllegalArgumentException if {@code messageTemplate} is {@code null}.
+   */
+  @Contract("_, null, _ -> fail; _, _, _ -> new")
+  public static @NotNull IllegalStateException illegalStateOf(
+      @Nullable final String key,
+      final MessageTemplates.TwoArgTemplate messageTemplate,
+      @Nullable final String value) {
+    return exceptionOf(IllegalStateException.class, key, messageTemplate, value);
+  }
 }
