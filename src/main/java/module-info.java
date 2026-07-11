664d505985c717eec6d7e9e491de120feea77b99
Christopher Molin <28791817+Chrimle@users.noreply.github.com>
2026-07-12 00:10:43 +0200

    Export `io.github.chrimle.exceptionfactory` Module (#85)
    Modularizes the project and exports it as `io.github.chrimle.exceptionfactory`. **NOTE**: This is **MAY** be a **breaking change**, if the default `exception.factory` module name is used. If so, rename the `require` to use the new official module name.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/** Create {@code Exception}-instances with Factory-methods and Builder patterns! */
+module io.github.chrimle.exceptionfactory {
+  // Exports
+  exports io.github.chrimle.exceptionfactory;
+
+  // Requires (static)
+  requires static org.jetbrains.annotations;
+  requires static org.jspecify;
+}
